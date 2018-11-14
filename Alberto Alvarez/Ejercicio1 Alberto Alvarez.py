53f6188518b75ec19522524cc5395104941b2c7d
alberto <alberto.a.e.g.m@hotmail.com>
2018-11-14 19:57:43 +0100

    primera subida

---
+++ Alberto Alvarez/Ejercicio1 Alberto Alvarez.py
@@ -0,0 +1,15 @@
+import math # esto es para poder hacer la raiz
+num1=0;
+num2=0;
+
+num1=int(input("introduce un numero"));
+num2=int(input("introduce un numero"));
+
+print("La suma es ",num1 + num2); #suma
+print("La resta es ",num1 - num2); #resta
+print("La division es ",num1 / num2); #division
+print("La raiz cuadrada",math.sqrt(num1)); #cuadrada de num1
+print("La raiz cuadrada",math.sqrt(num2)); #cuadrada de num2
+print("El cubo del primero es ",num1 ** 3); #cubo
+print("El cuadrado del segundo es  ",num2 ** 2);#cuadrado 
+
